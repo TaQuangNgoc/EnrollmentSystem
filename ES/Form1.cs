12182e8d9cd117d406398305496c26f297f8a595
Pham Tuan Anh <anhphamkstn@gmail.com>
2015-12-16 00:47:45 +0700

    khởi tạo entity và thêm thử môn học vào bảng Subject.

---
+++ ES/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // khởi tạo entity và thêm thử môn học vào bảng Subject.
+            var entities = new ESGEntities();
+            Subjects subject = new Subjects() { ID = 1, SubjectName = "Math" };
+            entities.Subjects.Add(subject);
+            entities.SaveChanges();
+        
+        }
+    }
+}
